3c13cc0a79d0ba2e9bab9b186d66a8638c0b1865
Lin Fanny <jenny87082446@gmail.com>
2023-10-05 17:38:02 +0800

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+// Step 1: Import React
+import * as React from "react";
+// import { Link } from "gatsby";
+import Layout from "../components/layout";
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </Layout>
+  );
+};
+
+//此行可以提高SEO搜尋引擎分析此網站
+export const Head = () => <title>About Me</title>;
+// Step 3: Export your component
+export default AboutPage;
